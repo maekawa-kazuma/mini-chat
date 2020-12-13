38667271718330e49d53f2cd16e3c06ff315315a
maekawa <kazumaekawa7218@gmail.com>
2020-12-13 18:09:09 +0900

    ルーティング編集（devise_for)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
   root to: 'users#index'
   resources :users, only: [:new]
   resources :rooms, only: [:index, :show]

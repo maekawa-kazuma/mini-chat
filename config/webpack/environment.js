d107fe5990de227ded96d0ebe83d4d0475289876
maekawa <kazumaekawa7218@gmail.com>
2020-12-14 21:57:34 +0900

    install_jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
+// jqueryの導入
+const webpack = require('webpack');
+
+environment.plugins.append('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+);
+
+// jqueryの導入おわり
+
 module.exports = environment

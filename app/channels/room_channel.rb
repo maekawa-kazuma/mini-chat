f129c67c14ff0fff1981dcc440ea123c27cb3beb
maekawa <kazumaekawa7218@gmail.com>
2020-12-15 17:06:03 +0900

    update rooms_file ( action cable)

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -3,7 +3,7 @@ class RoomChannel < ApplicationCable::Channel
     # Consumer(WebSocketのクライアント)がChannelをsubscribe（受信）することでsubscriberとして振る舞う。
 
   def subscribed
-    stream_from "room_channel"
+    stream_from "room_channel_#{params['room']}"
     # Streamすることで、Channelにルーティング機能を与える。
   end
 
@@ -14,6 +14,6 @@ class RoomChannel < ApplicationCable::Channel
   def speak(data)
     # jsで実行されたspeakのmessageを受け取り、room_channelのreceivedにブロードキャストする
     # ActionCable.server.broadcast 'room_channel', message: data['message']
-    Message.create! content: data['message']
+    Message.create! content: data['message'], user_id: current_user.id, room_id: params['room']
   end
 end

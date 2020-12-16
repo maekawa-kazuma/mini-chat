742e36769e40cfcd737fe5804721369ad2797ccd
maekawa <kazumaekawa7218@gmail.com>
2020-12-16 23:08:48 +0900

    Update message.rb (include Elasticsearch::Model)

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
+  include Elasticsearch::Model
+
   validates :content, presence: true
     # createの後(データ作成後）にコミット { MessageBroadcastJobのperformを遅延実行 引数はself }
     after_create_commit { MessageBroadcastJob.perform_later self }
